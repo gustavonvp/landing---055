2952be7686b5323387bd922d26191b4f65dc225f
Gustavonvp <gustavonvp@gmail.com>
2022-01-16 10:48:34 -0300

    getting the page from  api

--- src/app/news-api/na-article-list/na-article-list.component.ts
+++ src/app/news-api/na-article-list/na-article-list.component.ts
@@ -16,6 +16,8 @@ export class NaArticleListComponent implements OnInit {
     this.newsApiService.pagesOutput.subscribe((articles) => {
       this.articles = articles;
     })
+
+    this.newsApiService.getPage(1)
   }
 
   ngOnInit() {
