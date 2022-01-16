45f8827bc481aacd8e16249b2d5c488f67e6e411
Gustavonvp <gustavonvp@gmail.com>
2022-01-16 11:33:30 -0300

    interpolation, with a pipe component and props of api

--- src/app/news-api/trim-outlet-name.pipe.ts
+++ src/app/news-api/trim-outlet-name.pipe.ts
@@ -5,8 +5,8 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class TrimOutletNamePipe implements PipeTransform {
 
-  transform(value: any, ...args: any[]): any {
-    return value + "    --- new pipe";
+  transform(title: string, outletName:string): any {
+    return title.replace(` - ${outletName}`, '');
   }
 
 }
